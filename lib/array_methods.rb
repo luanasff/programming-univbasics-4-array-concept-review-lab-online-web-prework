c990d57411875ef0ef878fd106936b7998da62e6
Luana Faria <luanasfff@gmail.com>
2019-10-21 21:37:57 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,13 +1,18 @@
 def find_element_index(array, value_to_find)
-counter =0
-while counter < array.length do
-  if array[counter] == value_to_find
-    return counter
-  end
-  counter +=1
- end
-nil 
-end  
+while 
+legth= array.legth
+
+length.times do |counter|
+
+#counter =0
+#while counter < array.length do
+#  if array[counter] == value_to_find
+#    return counter
+#  end
+#  counter +=1
+# end
+#nil 
+#end  
 
 
 
